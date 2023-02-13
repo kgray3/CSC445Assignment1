df74914e7d49d38c8bf9f8035e1d34bb015e76b1
kgray3 <60368187+kgray3@users.noreply.github.com>
2023-02-13 11:50:07 -0500

    hardcoded server host

--- UDPClient.java
+++ UDPClient.java
@@ -5,17 +5,12 @@ import java.util.*;
 public class UDPClient {
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 1) {
-            System.out.println("Usage: java UDPClient <hostname>");
-            return;
-        }
-
             // get a datagram socket
         DatagramSocket socket = new DatagramSocket();
 
             // send request
         byte[] buf = new byte[256];
-        InetAddress address = InetAddress.getByName(args[0]);
+        InetAddress address = InetAddress.getByName("localhost");
         DatagramPacket packet = new DatagramPacket(buf,buf.length,address,3000);
         socket.send(packet);
 
@@ -25,5 +20,9 @@ public class UDPClient {
 
             // display response
         String received = new String(packet.getData(), 0, packet.getLength());
-    }    
+        System.out.println("Quote of the Moment: " + received);
+
+        socket.close();
+    }
+    
 }
